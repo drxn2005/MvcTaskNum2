64ce041a86552acd85a2d32d33c69beabf5fe6ec
drxn2 <drxn2@T0P>
2025-11-05 21:02:44 +0200

    Add project files.

---
+++ MvcTaskNum2/DataAccessApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcTaskNum2
+{
+    public class DataAccessApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=MvcTaskNum2Db;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+}
---
+++ MvcTaskNum2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcTaskNum2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
