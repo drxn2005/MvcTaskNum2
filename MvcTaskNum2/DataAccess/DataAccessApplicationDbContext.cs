ebd2f22fe9236207721fb0ff4be36bb12c4035b0
Mohamed Montaser <drxn2005@gmail.com>
2025-11-05 22:09:55 +0200

    updates

---
+++ MvcTaskNum2/DataAccess/DataAccessApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcTaskNum2.Models;
+
+namespace ToDoList518.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<TodoTask> TodoTasks { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=.; Initial Catalog= ToDoTask521 ;Integrated Security=True;Encrypt=True;Trust Server Certificate=True;");
+
+        }
+    }
+}
+
---
+++ MvcTaskNum2/Models/TodoTask.cs
@@ -0,0 +1,11 @@
+﻿namespace MvcTaskNum2.Models
+{
+    public class TodoTask
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime DeadLine { get; set; }
+        public string? FilePath { get; set; }
+    }
+}
